2cc08fddd35da1634f00a4fb3418dac223277436
devayanijs16 <63870564+devayanijs16@users.noreply.github.com>
2020-04-19 23:57:28 +0530

    Add files via upload

---
+++ java partb 2/program 5/DBConnection.java
@@ -0,0 +1,29 @@
+package jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DBConnection {
+	public Connection getDBconnection()
+	{
+		Connection conn=null;
+		try 
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Registered successfully");
+		} 
+		catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		try 
+		{
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/EMP","djs","root123");
+			System.out.println("Connection successfull\n");
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}	
+		return conn;		
+	}
+}
